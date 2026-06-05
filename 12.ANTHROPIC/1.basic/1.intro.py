bee75294b6ed25b60a441e12c1e60853d298020a
황동건 <guugun@naver.com>
2026-06-05 14:15:11 +0900

    [update]260605 중간커밋

--- 12.ANTHROPIC/1.basic/1.intro.py
+++ 12.ANTHROPIC/1.basic/1.intro.py
@@ -1,36 +1,24 @@
 # pip install anthropic
-
 import os
+
+# import openai
 import anthropic
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
+# client = openai.OpenAI()
 client = anthropic.Anthropic()
 
-def ask(question):
-    print("질문: ", question)
-
-    messages.append({"role": "user", "content": question})
-
-    message = client.messages.create(
-        #haiku, sonnet, opus
-        model="claude-haiku-4-5",
-        max_tokens=300,
-        messages=messages
-    )
+# message = client.chat.completion
+message = client.messages.create(
+    # haiku(빠름), sonnet, opus(최신, 고성능)
+    model="claude-haiku-4-5",
+    max_tokens=300,
+    messages=[{
+        "role":"user", "content": "안녕! 한 문장으로 너를 소개해줘."
+    }]
+)
 
 print(message.content[0].text)
-
-
-
-
-
-
-
-
-
-
-
-
-
